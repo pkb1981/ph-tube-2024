7c919594d517cc73043bff3c9d9f32293450df3b
Pallab Biswas <pallab.b7@gmail.com>
2023-11-30 15:25:27 +0600

    model type button section

--- assignment-6.js
+++ assignment-6.js
@@ -1,1 +1,21 @@
+// model type button section loading and displaying
+const loadCategory = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+    const allCategory = data.data;
+    displayCategory(allCategory);
+}
 
+const displayCategory = (categories) => {
+    const modelCategory = document.getElementById('model-category');
+    categories.forEach(category => {
+        console.log(category);
+        const modelDiv = document.createElement('div');
+        modelDiv.innerHTML = `
+        <button class="btn btn-neutral btn-sm capitalize ">${category.category}</button >
+        
+    `;
+        modelCategory.appendChild(modelDiv);
+    });
+}
+loadCategory();
