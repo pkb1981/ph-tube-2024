0f35ef30a2ac133b2cb696ebf05707ad9484a529
Pallab Biswas <pallab.b7@gmail.com>
2023-12-01 10:41:48 +0600

    time added

--- assignment-6.js
+++ assignment-6.js
@@ -12,7 +12,7 @@ const displayCategory = (categories) => {
         // console.log(category);
         const modelDiv = document.createElement('div');
         modelDiv.innerHTML = `
-        <button class="btn btn-neutral btn-sm capitalize ">${category.category}</button >
+        <button class="btn btn-neutral btn-sm capitalize">${category.category}</button >
         `;
         modelCategory.appendChild(modelDiv);
     });
@@ -37,14 +37,26 @@ const showAllModel = (models) => {
         const newModelDiv = document.createElement('div');
         newModelDiv.classList = `card bg-gray-100 shadow-xl`;
         newModelDiv.innerHTML = `
-        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-                </figure>
+        <div class="relative">
+        <figure><img src="${model.thumbnail}" alt="models" />
+        </figure>
+        <div class="absolute bottom-1 right-1 transform -translate-x-1/2 -translate-y-1/2 text-white text-md font-semibold bg-slate-600 rounded-sm">
+        <p>${model.others.posted_date}</p>
+        </div>
+        </div>
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                <div class="flex gap-2">
+                <image src="${model.authors[0].profile_picture}" class="h-10 w-10 rounded-full" alt="images"/>
+                <h1 class="font-medium">${model.title}</h1>
+                </div>
+                
+                       
+                    <div class="flex justify-center items-center gap-2">
+                    <h2 class="text-sm text-center">${model.authors[0].profile_name}</h2>
+                    <p>${model?.authors[0]?.verified}</p>
                     </div>
+                    <p>${model.others.views} views</p>
+                    
                 </div>
         `;
         allModelContainer.appendChild(newModelDiv);
